45439442ddfaf2f49c08376b41e84196e5155cda
Gntlgenius <shadrachmaduako@gmail.com>
2021-04-17 20:05:52 +0100

    added pytest an tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version ="0.0.1",
+    description ="this is a wine q package",
+    author = "Gentlecode",
+    packages = find_packages(),
+    license = "MIT"
+)
