efd2d27b0f376a7e798da22d6c816e5a199c768b
Gntlgenius <shadrachmaduako@gmail.com>
2021-04-18 14:21:10 +0100

    NotInRange exception added in pytest

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,15 @@
+import pytest
+
+
+class NotInRange(Exception):
+    def __init__(self, message="Value not in Range"):
+        self.message=message
+        super().__init__(self.message)
 
 
 def test_generic():
-    a=2
-    b=2
-
-    assert a==b
+        a=2
+        with pytest.raises(NotInRange):
+            if a not in range(7, 14):
+                raise NotInRange
+  
