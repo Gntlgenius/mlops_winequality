ecc56b218359c08ca4f9d5e54caffefa1bd6280c
Gntlgenius <shadrachmaduako@gmail.com>
2021-04-19 13:05:04 +0100

    app created and tested

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import numpy as np
-from prediction_service import prediction
+from prediction_service import prediction, predict_mgt
+
 
 
 params_path = "params.yaml"
@@ -15,7 +16,21 @@ app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 @app.route("/", methods =['POST','GET'])
 def home():
     if request.method =="POST":
-        pass
+        try:
+            if request.form:
+                values = dict(request.form).values()
+                data = [list(map(float, values))]
+                response = predict_mgt.predict(data)
+                return render_template("index.html", response=response)
+            elif request.json:
+                response =predict_mgt.api_response(request)
+                return jsonify(response)
+                
+        except Exception as e:
+            print(e)
+            error = {"error":"something went wrong"}
+            return render_template("404.html", error = error)
+
     else:
         return render_template("index.html")
 
