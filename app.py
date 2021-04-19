e44332f0b0ea722f72fe0e13fc993951aaa67927
Gntlgenius <shadrachmaduako@gmail.com>
2021-04-19 21:22:34 +0100

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import numpy as np
-from prediction_service import predict_mgt
+from src.get_data import read_params
+import joblib
+
+#from prediction_service import predict_mgt
 
 
 
@@ -13,6 +16,25 @@ template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
+def predict(data):
+    config = read_params(params_path)
+    model_path = config["model_dir"]
+    model = joblib.load(model_path)
+    prediction = model.predict(data)
+    return prediction[0]
+
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json.values())])
+        response = predict(data)
+        response = {'response':response}
+        return response
+    except Exception as e:
+        print (e)
+        error = {"error":"Something went wrong in api_response"}
+        return error
+
 @app.route("/", methods =['POST','GET'])
 def home():
     if request.method =="POST":
@@ -20,10 +42,10 @@ def home():
             if request.form:
                 values = dict(request.form).values()
                 data = [list(map(float, values))]
-                response = predict_mgt.predict(data)
+                response = predict(data)
                 return render_template("index.html", response=response)
             elif request.json:
-                response =predict_mgt.api_response(request)
+                response = api_response(request)
                 return jsonify(response)
                 
         except Exception as e:
